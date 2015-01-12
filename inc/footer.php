cfd85d644c5db488fb8c4aefaf1cf35f2641f02d
Andrew Klatzke <klatzkaj@muohio.edu>
2015-01-12 10:33:34 -0500

    remove everything so that we can clear the document revision history

--- inc/footer.php
+++
@@ -1,8 +0,0 @@
-	</div> <!-- End Container -->
-	<!-- Load Javascript -->
-    <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script type="text/javascript" src="js/uikit.min.js"></script>
-    <script type="text/javascript" src="js/upload.min.js"></script>
-    <script type="text/javascript" src="js/scripts.js"></script>
-</body>
-</html>
